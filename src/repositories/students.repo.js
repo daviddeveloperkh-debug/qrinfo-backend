204e5199e18bea7554626763d47a8f91ad7558c5
Begaliev Bekzod <bek77519101@gmail.com>
2026-04-24 09:33:45 +0500

    fix

--- src/repositories/students.repo.js
+++ src/repositories/students.repo.js
@@ -107,7 +107,7 @@ const createStudent = async (requestData, currentYear) => {
 		.insert(students)
 		.values({
 			...requestData,
-			year: currentYear,
+			year: Number(currentYear),
 		})
 		.returning()
 	return student
